5f52f93b0dfde6af5449965759216909d63f2df7
Shin Uesugi <shinsaku417@gmail.com>
2015-02-03 19:47:56 -0800

    (feat) Added facebook authentication and passport node modules

--- server/middleware.js
+++ server/middleware.js
@@ -1,25 +1,32 @@
 var morgan      = require('morgan'), // used for logging incoming request
     bodyParser  = require('body-parser'),
-    helpers     = require('./helpers.js'); // our custom middleware
-var multiparty = require('multiparty');
+    helpers     = require('./helpers.js'), // our custom middleware
+    multiparty = require('multiparty'),
+    passport = require('passport'),
+    FacebookStrategy = require('passport-facebook').Strategy,
+    session = require('express-session'),
+    cookieParser = require("cookie-parser");
 
 module.exports = function (app, express) {
   // Express 4 allows us to use multiple routers with their own configurations
   var userRouter = express.Router();
   var groupRouter = express.Router();
   var clientRouter = express.Router();
-  var facebookRouter = express.Router();
+  var facebookController = express.Router();
 
   app.use(morgan('dev'));
+  app.use(cookieParser());
   app.use(bodyParser.urlencoded({extended: true}));
   app.use(bodyParser.json({type: 'application/*+json'}));
+  app.use(session({ secret: 'keyboard cat'}));
+  app.use(passport.initialize());
+  app.use(passport.session());
   app.use(express.static(__dirname + '/../client'));
-
 
   app.use('/api/users', userRouter); // use user router for all user request
   app.use('/api/groups', groupRouter); // use group router for group request
   app.use('/api/clients', clientRouter); // use client router for all client request
-  app.use('/auth/facebook', facebookRouter); // use facebook router for facebook authorization
+  app.use('/auth/facebook', facebookController); // use facebook controller for facebook authorization
 
 
   app.use(helpers.errorLogger);
@@ -29,5 +36,5 @@ module.exports = function (app, express) {
   require('./users/userRoutes.js')(userRouter);
   require('./groups/groupRoutes.js')(groupRouter);
   require('./clients/clientRoutes.js')(clientRouter);
-  require('/facebook/facebookRoutes.js')(facebookRouter);
+  require('./facebook/facebookController.js')(facebookController);
 };
