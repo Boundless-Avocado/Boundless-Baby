c26f0a566b6c214519317eb6e127fde723ef3712
Ryan McCarter <mccarter.ryan@gmail.com>
2015-02-06 13:42:52 -0800

    deletes server files outside of ionic app and adds server files inside of the ionic app

--- server/db/index.js
+++
@@ -1,8 +0,0 @@
-var Sequelize = require('sequelize');
-
-var db = new Sequelize('avocado', 'guacman', '', {
-  dialect: 'sqlite',
-  storage: './server/db/db.sqlite'
-});
-
-module.exports = db;
--- server/schema.sql
+++
@@ -1,7 +0,0 @@
-CREATE DATABASE avocado;
-
-# Create matching mysql user for development:
-CREATE USER 'guacman'@'localhost' IDENTIFIED BY 'Boundless1!';
-GRANT ALL ON avocado.* TO 'guacman'@'localhost';
-
-# TODO: tighten mysql user permissions to necessary only
